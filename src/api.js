f9f8d9a7775488bbb4849177c6908208e069e600
Stephen <stephensouness@outlook.com>
2024-10-09 21:32:54 +0100

    single product api call created in api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ async function getProducts() {
     const response = await fetch("https://fakestoreapi.com/products");
 
     if (!response.ok) {
-      throw new Error("Could not fetch product");
+      throw new Error("Could not fetch products");
     }
     const data = await response.json();
     console.log(data);
@@ -13,5 +13,23 @@ async function getProducts() {
     return [];
   }
 }
+
+async function getSingleProduct(productId) {
+  try {
+    const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+
+    if (!response.ok) {
+      throw new Error("Could not fetchi this product");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+}
+
+getSingleProduct();
 
 export default getProducts;
