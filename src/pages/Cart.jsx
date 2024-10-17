84eed29df458ccb4e8b9e90b2fc54e5ff867a74f
Stephen <stephensouness@outlook.com>
2024-10-17 19:38:54 +0100

    styled layout of card, added quantity buttons in cart but not quite connected to correct state yet

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getSingleProduct } from "../api";
 
-function Cart({ cart }) {
+function Cart({ cart, quantity, setQuantity }) {
   const [totalPrice, setTotalPrice] = useState(0);
 
   const calculateTotalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
@@ -12,23 +12,48 @@ function Cart({ cart }) {
     setTotalPrice(calculateTotalPrice);
   }, [cart, calculateTotalPrice]);
 
+  const handleMinusClick = () => {
+    if (quantity >= 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handlePlusClick = () => {
+    setQuantity(quantity + 1);
+  };
+
   return (
     <div className="cart-container">
       <h1>Your Shopping Cart</h1>
-
+      <div className="cart-headers">
+        <p>Item</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+      </div>
       <ul>
         {cart.map((product) => {
           return (
             <li key={product.id} className="cart-product-container">
-              <img src={product.image} alt="" />
               <div>
-                <h3>{product.title}</h3>
-                <div className="price-quantity-container">
-                  <p>Price: £{product.price}</p>
-                  <p>Quantity: {product.quantity}</p>
-                </div>
-                <p className="cart-subtotal">Subtotal: £{product.price * product.quantity}</p>
+                <h3 className="cart-product-title">
+                  {product.title.length > 25 ? `${product.title.substring(0, 25)}...` : product.title}
+                </h3>
+                <img className="cart-image" src={product.image} alt="" />
               </div>
+
+              <p className="cart-product-price">£{product.price}</p>
+
+              <div className="quantity-increment">
+                <button className="cart-increment-button" onClick={handleMinusClick}>
+                  -
+                </button>
+                <p>{product.quantity}</p>
+                <button className="cart-increment-button" onClick={handlePlusClick}>
+                  +
+                </button>
+              </div>
+              <p className="cart-subtotal">£{product.price * product.quantity}</p>
             </li>
           );
         })}
