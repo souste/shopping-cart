02a59197da9f62f6a58a0a9cf8294be32b1b3e19
Stephen <stephensouness@outlook.com>
2024-10-23 08:46:59 +0100

    resolved issue with the loader in shop page causing some weird render with the footer

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -15,35 +15,40 @@ function ShopPage() {
     });
   }, []);
 
-  if (loading) return <p>Loading...</p>;
-  // should I add an error here?
-
   return (
     <div className="shop-container">
-      <div className="shop-banner">
-        <div className="shop-banner-title-description">
-          <h2>All products</h2>
-          <p className="shop-banner-description">
-            Featuring all of our iconic store items. Shop here if this is where you want to blah blah blah
-          </p>
+      {loading ? (
+        <div className="loading-placeholder">
+          <p>Loading...</p>
         </div>
-        <img src={shopBannerPhoto} alt="shop-banner-woman-handbag" />
-      </div>
-      <ul className="products-container">
-        {products.map((product) => {
-          return (
-            <Link key={product.id} to={`/products/${product.id}`}>
-              <li className="product-container" key={product.id}>
-                <img src={product.image} alt={product.title} />
-                <h3 className="products-title">
-                  {product.title.length > 65 ? `${product.title.substring(0, 65)}...` : product.title}
-                </h3>
-                <p>£{product.price}</p>
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
+      ) : (
+        <>
+          <div className="shop-banner">
+            <div className="shop-banner-title-description">
+              <h2>All products</h2>
+              <p className="shop-banner-description">
+                Featuring all of our iconic store items. Shop here if this is where you want to blah blah blah
+              </p>
+            </div>
+            <img src={shopBannerPhoto} alt="shop-banner-woman-handbag" />
+          </div>
+          <ul className="products-container">
+            {products.map((product) => {
+              return (
+                <Link key={product.id} to={`/products/${product.id}`}>
+                  <li className="product-container" key={product.id}>
+                    <img src={product.image} alt={product.title} />
+                    <h3 className="products-title">
+                      {product.title.length > 65 ? `${product.title.substring(0, 65)}...` : product.title}
+                    </h3>
+                    <p>£{product.price}</p>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
